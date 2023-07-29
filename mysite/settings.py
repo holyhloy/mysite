ba6f6ec5aba9e43e01267cfd4ff3477cb9bafa28
holyhloy <tlegchilin@yandex.ru>
2023-07-29 19:54:23 +0300

    Safety, Comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,5 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
