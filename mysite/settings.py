dd13123044a5e6aacf313277e039f51eee214a14
holyhloy <tlegchilin@yandex.ru>
2023-07-29 17:24:22 +0000

    Some error with settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR / 'media')
